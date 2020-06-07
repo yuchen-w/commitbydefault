191b7d234a48407a853dff0c7edd7f4c9d842b21
Yuchen Wang <yuchen_wng@hotmail.com>
2020-06-08 00:56:08 +0100

    Updates

---
+++ precache-manifest.54ed3b2b3d101d40a5d1bc10702cc6c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592647070db45c69dc04241016fba80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37635755962b8dfdad20",
+    "url": "/static/css/2.015ddf31.chunk.css"
+  },
+  {
+    "revision": "318f6b5e8a3e8490193e",
+    "url": "/static/css/main.5a653164.chunk.css"
+  },
+  {
+    "revision": "37635755962b8dfdad20",
+    "url": "/static/js/2.275dcd45.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.275dcd45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318f6b5e8a3e8490193e",
+    "url": "/static/js/main.3429e4ab.chunk.js"
+  },
+  {
+    "revision": "dd793b8fede7de2a900c",
+    "url": "/static/js/runtime-main.4d26dd67.js"
+  }
+]);
